43fc29a82450b8ba9bd19f4a77a6551abe25125b
Mher Sargsyan <msargsyan@msargsyan-mac-2.local>
2018-05-07 04:56:00 -0700

    Deleted files

--- test3.js
+++
@@ -1,9 +0,0 @@
-
-const sheetjs = require('xlsx')
-;
-
-const workbook1 = sheetjs.readFile('./lib/shows.xlsx');
-const firstSheetName = workbook1.SheetNames[0];
-const worksheet = workbook1.Sheets[firstSheetName];
-
-console.log(workbook1);
